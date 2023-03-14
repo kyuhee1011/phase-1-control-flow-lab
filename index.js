5249a99b8e445fe2be6c5054707ed5e51f25155d
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-03-14 10:15:56 -0400

    control lab finish

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber <= 400){
+  return "This one is on me!"}
+
+  else if (someNumber > 400 && someNumber < 2000 ){
+  return "That will be twenty bucks."}
+
+  else if (someNumber >2000 && someNumber <2500){
+  return 'I will gladly take your thirty bucks.'}
+
+  else if (someNumber > 2500){
+  return 'No can do.' };
+}
+ 
+function ternaryCheckCity(region){
+  // Write your code here!
+
+  if (region ==='NYC') {
+  return "Ok, sounds good."}
+  
+  else if(region !== 'NYC'){
+  return "No go."};
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(serviceTip){
   // Write your code here!
+
+  switch (serviceTip) {
+    case 'generous' :
+      return 'Thank you so much.'
+      case 'not as generous' :
+        return 'Thank you.'
+        case 'thanks for everything' : 
+        return 'Bye.'
+  }
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
